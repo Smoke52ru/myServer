6923544c46337766fca1f1bf664f42007adfb10b
Tema Lapin <Smoke52ru@ya.ru>
2023-01-27 21:05:06 +0300

    fix: Вернул в клавиатуру пн-пт, вместо пн-чт/сб

--- src/features/Bot/middlewares/keyboard/commands.ts
+++ src/features/Bot/middlewares/keyboard/commands.ts
@@ -4,6 +4,6 @@ import {weatherCommands} from "../weather";
 export const keyboardCommands: string[] = ['клавиатура', 'к', 'keyboard', 'k']
 
 export const keyboardButtonsDefault: string[][] = [
-  scheduleCommands.weekdays.filter((_, index) => (index !== 4)),
-  [scheduleCommands.today[0], scheduleCommands.tomorrow[0], weatherCommands[0]],
+    scheduleCommands.weekdays.filter((_, index) => (index < 5)), // с понедельника по пятницу - главные
+    [scheduleCommands.today[0], scheduleCommands.tomorrow[0], weatherCommands[0]],
 ]
